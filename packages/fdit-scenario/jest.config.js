4ed25730c0b8e62669598aa49d5e68dd4cfc0db8
Morgan <morgan.leclercq89@gmail.com>
2023-01-23 13:28:51 +0100

    update jest.config (remove execute test js)

--- packages/fdit-scenario/jest.config.js
+++ packages/fdit-scenario/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  testPathIgnorePatterns: ['./out/test/generator.test.js'],
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
