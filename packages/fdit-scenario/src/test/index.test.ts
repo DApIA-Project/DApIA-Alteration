640d9a2a88f1966251a2ab6ba6c0aff1cec83f06
Morgan <morgan.leclercq89@gmail.com>
2023-02-28 11:31:35 +0100

    modify index.test.ts and generator.ts, tests passed with mocha

--- packages/fdit-scenario/src/test/index.test.ts
+++ packages/fdit-scenario/src/test/index.test.ts
@@ -1,54 +1,59 @@
-import { parseAndGenerate } from "../web";
+import {parseAndGenerate} from "../web";
+import assert from "assert";
 
+describe("indexTestMocha", () => {
+    context('when scenario is no valid', () => {
+        it('returns json with no action when content is false', async () => {
 
-describe('indexTest', () => {
-    
-    
-    test('callGenerateCommandsHide', async () => {
-        const value = "hide all_planes from 56 seconds until 90 seconds";
-        // parse & generate commands for drawing an image
-        // execute custom LSP command, and receive the response
-        const cmds  = await parseAndGenerate(value,"");
-        const resJSONString = JSON.stringify(cmds, undefined, 2);
+            const value = "hide all_planes from seconds until 90 sconds";
+            // parse & generate commands for drawing an image
+            // execute custom LSP command, and receive the response
+            const cmds  = await parseAndGenerate(value,"");
+            assert.deepStrictEqual(cmds,undefined);
 
-        const resJson = JSON.parse(resJSONString);
-        expect(resJson).toStrictEqual([
-            {
-              "instructions": [
-                {
-                  "action": "DELETION",
-                  "target": [
-                    {
-                      "identifier" : "ALL",
-                      "filters" : []
-                    }
-                  ],
-                  "scope": [
-                    {
-                      "type": "timeWindow",
-                      "lowerBound": 56,
-                      "upperBound": 90
-                    }
-                  ],
-                  "trigger": [],
-                  "assertions": []
-                }
-              ]
-            }
-          ])
+        })
     })
 
 
-    test('callGenerateCommandsHideError', async () => {
-      const value = "hide all_planes from seconds until 90 sconds";
-      // parse & generate commands for drawing an image
-      // execute custom LSP command, and receive the response
-      const cmds  = await parseAndGenerate(value,"");
-      const resJSONString = JSON.stringify(cmds, undefined, 2);
+    context('when scenario is valid', () => {
+        it('returns json with action when content is hide all_planes from until', async () => {
+            const value = "hide all_planes from 56 seconds until 90 seconds";
+            // parse & generate commands for drawing an image
+            // execute custom LSP command, and receive the response
+            const cmds  = await parseAndGenerate(value,"");
+            const resJSONString = JSON.stringify(cmds, undefined, 2);
 
-      const resJson = JSON.parse(resJSONString);
-      expect(resJson).toStrictEqual([
-          "Erreur de syntaxe"
-        ])
-  })
+            const resJson = JSON.parse(resJSONString);
+            assert.deepStrictEqual(resJson,
+                {
+                    "sensors": {
+                        "sensor": [
+                            {
+                                "sensorType": "SBS",
+                                "sID": "",
+                                "record": "",
+                                "filter": "",
+                                "action": [
+                                    {
+                                        "alterationType": "DELETION",
+                                        "scope": {
+                                            "type": "timeWindow",
+                                            "lowerBound": "56000",
+                                            "upperBound": "90000"
+                                        },
+                                        "parameters": {
+                                            "target": {
+                                                "identifier": "hexIdent",
+                                                "value": "ALL"
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            )
+        })
+    })
 })
