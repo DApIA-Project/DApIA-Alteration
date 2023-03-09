16dd96f4a57736be6dae2d0ad2e4726e1ad3239d
Morgan <morgan.leclercq89@gmail.com>
2023-03-09 10:11:14 +0100

    modify html

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles.css';
+import monImage from './assets/aircraft_white.png';
 function App() {
 
   return (
       <div className="App">
-      <h1>FDIT</h1>
+          <div className={"header"}>
+              <img src={monImage} alt={"aircraft"}/>
+              <h1 className={"title"}>FDIT-Web</h1>
+          </div>
       <div id="page-wrapper">
           <div className="half">
               <div className="wrapper">
@@ -14,6 +18,7 @@ function App() {
         <div className="half">
           <div className="wrapper">
             <pre id="zoneJson"></pre>
+
           </div>
         </div>
       </div>
