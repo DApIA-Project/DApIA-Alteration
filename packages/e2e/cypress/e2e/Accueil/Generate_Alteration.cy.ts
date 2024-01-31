ae3354ab4e394878d17e21680c0bec86f65ece36
Morgan LECLERCQ <morgan.leclercq89@gmail.com>
2024-01-31 11:23:45 +0100

    fix : modify test generate alteration cypress

--- packages/e2e/cypress/e2e/Accueil/Generate_Alteration.cy.ts
+++ packages/e2e/cypress/e2e/Accueil/Generate_Alteration.cy.ts
@@ -3,43 +3,40 @@ describe('Generate a complete alteration', () => {
   beforeEach(() => {
     cy.task('db:reset')
     cy.visit('http://localhost:3000')
-    cy.wait(1000)
     cy.get('[data-testid="AddTabButton"]').click()
-    cy.get('[class="alterationeditor"]').click()
   })
 
   it('Generate an alteration without option', () => {
-    cy.get('[class="alterationeditor"]').click()
-    cy.get('[class="alterationeditor"]').type('hide all_planes at 5 seconds')
+    cy.get('[class="view-line"]').type('hide all_planes at 5 seconds')
     cy.get(
       '[data-testid="RecordInputFiles.action.selectRecording"]'
     ).selectFile('cypress/resources/2022_07_toulouse_SAMUCF_1h.sbs')
     cy.get('[class="imageDownload"]').should('not.exist')
     cy.get('[data-testid="GenerateAlterationButton"]').click()
-    cy.wait(5000)
-    cy.get('[class="imageDownload"]').should(($elements) => {
-      expect($elements).to.have.length(1)
-    })
+    cy.get('[class="imageDownload"]', { timeout: 20000 }).should(
+      ($elements) => {
+        expect($elements).to.have.length(1)
+      }
+    )
   })
 
   it('Generate an alteration with option', () => {
-    cy.get('[class="alterationeditor"]').click()
-    cy.get('[class="alterationeditor"]').type('hide all_planes at 5 seconds')
+    cy.get('[class="view-line"]').type('hide all_planes at 5 seconds')
     cy.get('[data-testid="Labeling"]').click()
     cy.get(
       '[data-testid="RecordInputFiles.action.selectRecording"]'
     ).selectFile('cypress/resources/2022_07_toulouse_SAMUCF_1h.sbs')
     cy.get('[class="imageDownload"]').should('not.exist')
     cy.get('[data-testid="GenerateAlterationButton"]').click()
-    cy.wait(5000)
-    cy.get('[class="imageDownload"]').should(($elements) => {
-      expect($elements).to.have.length(1)
-    })
+    cy.get('[class="imageDownload"]', { timeout: 20000 }).should(
+      ($elements) => {
+        expect($elements).to.have.length(1)
+      }
+    )
   })
 
   it('Generate an alteration with variable', () => {
-    cy.get('[class="alterationeditor"]').click()
-    cy.get('[class="alterationeditor"]').type(
+    cy.get('[class="view-line"]').type(
       'let $var = {{}1,7{}}, hide all_planes at $var seconds\n'
     )
     cy.get(
@@ -47,10 +44,11 @@ describe('Generate a complete alteration', () => {
     ).selectFile('cypress/resources/2022_07_toulouse_SAMUCF_1h.sbs')
     cy.get('[class="imageDownload"]').should('not.exist')
     cy.get('[data-testid="GenerateAlterationButton"]').click()
-    cy.wait(5000)
-    cy.get('[class="imageDownload"]').should(($elements) => {
-      expect($elements).to.have.length(2)
-    })
+    cy.get('[class="imageDownload"]', { timeout: 20000 }).should(
+      ($elements) => {
+        expect($elements).to.have.length(2)
+      }
+    )
   })
 
   it('Generate an alteration in tab 6', () => {
@@ -60,31 +58,31 @@ describe('Generate a complete alteration', () => {
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
-    cy.get('[class="alterationeditor"]').click()
-    cy.get('[class="alterationeditor"]').type('hide all_planes at 5 seconds')
+    cy.get('[class="view-line"]').type('hide all_planes at 5 seconds')
     cy.get(
       '[data-testid="RecordInputFiles.action.selectRecording"]'
     ).selectFile('cypress/resources/2022_07_toulouse_SAMUCF_1h.sbs')
     cy.get('[class="imageDownload"]').should('not.exist')
     cy.get('[data-testid="GenerateAlterationButton"]').click()
-    cy.wait(5000)
-    cy.get('[class="imageDownload"]').should(($elements) => {
-      expect($elements).to.have.length(1)
-    })
+    cy.get('[class="imageDownload"]', { timeout: 20000 }).should(
+      ($elements) => {
+        expect($elements).to.have.length(1)
+      }
+    )
   })
 
   it("Don't generate alteration because error in scenario", () => {
-    cy.get('[class="alterationeditor"]').click()
-    cy.get('[class="alterationeditor"]').type('hide all_planes at 5 second')
+    cy.get('[class="view-line"]').type('hide all_planes at 5 second')
     cy.get(
       '[data-testid="RecordInputFiles.action.selectRecording"]'
     ).selectFile('cypress/resources/2022_07_toulouse_SAMUCF_1h.sbs')
     cy.get('[class="imageDownload"]').should('not.exist')
     cy.get('[data-testid="GenerateAlterationButton"]').click()
-    cy.get('[class="imageDownload"]').should('not.exist')
-    cy.wait(5000)
-    cy.get('[data-testid="ScenarioOutput.action.displayError"]')
+    cy.get('[data-testid="ScenarioOutput.action.displayError"]', {
+      timeout: 20000,
+    })
       .should('exist')
       .should('be.visible')
+    cy.get('[class="imageDownload"]').should('not.exist')
   })
 })
