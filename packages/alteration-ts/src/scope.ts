47e278daadf6932a99ad96c02c10e1409d39cbb6
Aymeric <aymeric.cretin@smartesting.com>
2024-02-07 14:22:35 +0100

    Fix test run

--- packages/alteration-ts/src/scope.ts
+++ packages/alteration-ts/src/scope.ts
@@ -1,20 +1,19 @@
-import { Scope, Icao } from './types'
+import { Icao, Scope } from './types'
 
-
-export const always : Scope = (_) => true;
-export const never  : Scope = (_) => false;
+export const always: Scope = (_) => true
+export const never: Scope = (_) => false
 
 export function and(...scopes: Scope[]): Scope {
-	if(scopes.length == 0) return never;
-	return (msg) => scopes.reduce((acc, scope) => acc && scope(msg), true);
+  if (scopes.length == 0) return never
+  return (msg) => scopes.reduce((acc, scope) => acc && scope(msg), true)
 }
 
 export function target(hexIdent: Icao): Scope {
-	return (msg) => msg.hexIdent === hexIdent;
+  return (msg) => msg.hexIdent === hexIdent
 }
 
 export function timeWindow(lower_bound: number, upper_bound: number): Scope {
-	return (msg) => msg.timestampGenerated <= upper_bound 
-							 && msg.timestampGenerated >= lower_bound;
+  return (msg) =>
+    msg.timestampGenerated <= upper_bound &&
+    msg.timestampGenerated >= lower_bound
 }
-
