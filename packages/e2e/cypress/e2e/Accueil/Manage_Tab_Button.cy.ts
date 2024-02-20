fa8cc13f881c9e7e330e5fdb04d0c655baa63179
Morgan LECLERCQ <morgan.leclercq89@gmail.com>
2024-02-20 14:01:37 +0100

    fix : modify test component

--- packages/e2e/cypress/e2e/Accueil/Manage_Tab_Button.cy.ts
+++ packages/e2e/cypress/e2e/Accueil/Manage_Tab_Button.cy.ts
@@ -1,19 +1,19 @@
 export {}
+
 describe('Click on tab button', () => {
   beforeEach(() => {
     cy.task('db:reset')
     cy.visit('http://localhost:3000')
+    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[class="view-line"]').should('be.visible')
   })
 
   it('click on add tab button', () => {
     cy.get('[data-testid="AddTabButton"]').click()
-    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="DivTab"]').should('have.length', 2)
   })
 
   it('click on remove tab 2 button', () => {
-    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="DivTab"]').should('have.length', 2)
     cy.get('[data-testid="RemoveTabButton"]').eq(1).click()
@@ -22,14 +22,12 @@ describe('Click on tab button', () => {
 
   it('click on remove tab 1 button', () => {
     cy.get('[data-testid="AddTabButton"]').click()
-    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="DivTab"]').should('have.length', 2)
     cy.get('[data-testid="RemoveTabButton"]').eq(0).click()
     cy.get('[data-testid="DivTab"]').should('have.length', 1)
   })
 
   it('Write scenario in many tab and scenarios are saved', () => {
-    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="DivTab"]').should('have.length', 3)
@@ -48,7 +46,6 @@ describe('Click on tab button', () => {
   it('Write scenario in many tab and delete tab empty and scenarios are saved', () => {
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
-    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="DivTab"]').should('have.length', 3)
     cy.get('[data-testid="DivTab"]').eq(0).click()
     cy.get('[class="alterationeditor"]').click()
@@ -64,7 +61,6 @@ describe('Click on tab button', () => {
   })
 
   it('Write scenario in many tab and delete tab with scenario', () => {
-    cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="AddTabButton"]').click()
     cy.get('[data-testid="DivTab"]').should('have.length', 3)
