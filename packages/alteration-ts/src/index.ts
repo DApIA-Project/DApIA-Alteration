1af30f474a2b4d46fcde9713f2cd8368a1c90ceb
Lois Ecoffet <lois.ecoffet@edu.univ-fcomte.fr>
2024-03-01 15:51:00 +0100

    Add Earth functions

--- packages/alteration-ts/src/index.ts
+++ packages/alteration-ts/src/index.ts
@@ -4,6 +4,7 @@ export * from "./actions"
 export * from "./scope"
 export * from "./engine"
 export * from "./aircraftTrajectory"
+export * from "./utils"
 export * from "./template"
 
 import { Message } from './types'
@@ -12,9 +13,22 @@ export function clone(arr: readonly Message[]) {
 	return arr.map((x: Message) => {return {...x}});
 }
 
-// Formula from https://geophydog.cool/post/geometry_on_a_sphere/
+/**
+ * Compute earth distance between two point in meter
+ * to have meters we should multiply the angle distance by the radius of the globe
+ * see : 
+ * https://worldwind.arc.nasa.gov/java/v2.1.0/javadoc/gov/nasa/worldwind/geom/LatLon.html#greatCircleDistance-gov.nasa.worldwind.geom.LatLon-gov.nasa.worldwind.geom.LatLon-
+ */ 
 export function earth_distance(lat1: number, lon1: number, lat2: number, lon2: number){
 	const R = 6371e3;
+	return R * earth_distance_angle(lat1,lon1,lat2,lon2);
+}
+
+/**
+ * Compute the arc distance (radian) between two points
+ */
+// Formula from https://geophydog.cool/post/geometry_on_a_sphere/
+export function earth_distance_angle(lat1: number, lon1: number, lat2: number, lon2: number) {
 	// Values are in radians
 	const phi1 = lat1 * Math.PI/180; 
 	const phi2 = lat2 * Math.PI/180;
@@ -27,9 +41,12 @@ export function earth_distance(lat1: number, lon1: number, lat2: number, lon2: n
 	
 	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-	return R * c;
+	return c;
 }
 
+/**
+ * Compute the angle (degree) between two point
+ */ 
 // Formula from https://geophydog.cool/post/geometry_on_a_sphere/
 export function earth_azimuth(lat1: number, lon1: number, lat2: number, lon2: number) {
 	const phi1 = lat1 * Math.PI/180;
@@ -44,7 +61,41 @@ export function earth_azimuth(lat1: number, lon1: number, lat2: number, lon2: nu
 	return (((alpha * 180 ) / Math.PI) + 360) % 360;
 }
 
+/**
+ * Computes the location on a great circle arc with the given starting location, azimuth, and arc distance.
+ * Function from : https://github.com/nasa/World-Wind-Java/blob/master/WorldWind/src/gov/nasa/worldwind/geom/LatLon.java
+ * Equation from : "Map Projections - A Working Manual", page 31, equation 5-5 and 5-6. [ https://pubs.usgs.gov/pp/1395/report.pdf ]
+ *
+ * @param lat1,lon1 : start position (degree)
+ * @param az: azimuth angle (clockwise from North) (radian)
+ * @param c, arc distance to travel (radian)
+ * @return [lat,lon] : end position in degree
+ */ 
 
+export function earth_end_position(lat1: number, lon1: number, az: number, c: number) {
+	// Convert degree to radians
+	const phi1 = lat1 * Math.PI/180;
+	const lambda1 = lon1 * Math.PI/180;
+
+	// Equation 5-5 p.31
+	const phi = Math.asin(Math.sin(phi1) * Math.cos(c) + 
+												Math.cos(phi1) * Math.sin(c) * Math.cos(az));
+	
+	// Equation 5-6, p.31
+	const lambda = lambda1 + Math.atan2(Math.sin(c) * Math.sin(az),
+																			Math.cos(phi1) * Math.cos(c) - Math.sin(phi1) * Math.sin(c) * Math.cos(az));
+
+	return [
+		(phi * 180) / Math.PI,
+		(lambda * 180) / Math.PI
+	]
+}
+
+
+/**
+ * Operation with a fixed floating point number precission 
+ * Default precision is 2 (3.1234567890001 => 0 3.12)
+ */ 
 type Op = "-" | "+" | "*" | "/" 
 export function op(op: Op, lhs: number, rhs: number, n = 2) {
 	let result: number;
---
+++ packages/alteration-ts/src/utils/earth.ts
@@ -0,0 +1,121 @@
+
+
+export let to_degree = (r: number) => (r * 180) / Math.PI;
+export let to_radian = (d: number) => (d * Math.PI) / 180;
+
+export class Angle {
+	deg: number
+	rad: number
+
+	private constructor(){
+		this.deg = this.rad = 0;
+	}
+
+	static degree(d: number): Angle {
+		let a = new Angle();
+		a.deg = d;
+		a.rad = to_radian(d);
+
+		return a;
+	}
+
+	static radian(r: number): Angle { 
+		let a = new Angle();
+		a.deg = to_degree(r);
+		a.rad = r;
+
+		return a;
+	}
+}
+
+type Position = [Angle, Angle];
+
+/**
+ * Return latitude (in degree) of a position (between -90° and 90°)
+ */ 
+export function lat(p: Postion) {
+	let lat = p[0].deg % 180;
+	if(lat > 90) 	return Angle.degree(180 - lat);
+	if(lat < -90) return Angle.degree(-180 - lat);
+								return Angle.degree(lat);
+}
+
+/**
+ * Return longitude (in degree) of a position (between -180° and 180)
+ */ 
+export function lon(p: Postion) {
+	let lon = p[1].deg % 180;
+	if(lon > 180) 	return Angle.degree(lon - 360);
+	if(lon < -180) 	return Angle.degree(360 + lon);
+									return Angle.degree(lon);
+}
+
+export class Earth {
+	static radius = 6371e3;
+
+	/**
+	 * Compute the arc distance (angle) from 'p' to 'q'
+	 * Formula from https://geophydog.cool/post/geometry_on_a_sphere/
+	 */ 
+	static distance_arc(p: Position, q: Position): Angle {
+		const phi1 = lat(p).rad;
+		const phi2 = lat(q).rad;
+		const delta_phi = phi2 - phi1;
+		const delta_lambda = (lon(q).rad - lon(p).rad);
+
+		const a = Math.sin(delta_phi / 2) * Math.sin(delta_phi / 2) +
+			Math.cos(phi1) * Math.cos(phi2) * 
+			Math.sin(delta_lambda / 2) * Math.sin(delta_lambda / 2);
+
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+		return Angle.radian(c);
+	}
+
+	/**
+	 * Compute the distance between 'p' and 'q' in meters
+	 */  
+	static distance_meters(p: Position, q: Position): number {
+		return this.radius * this.distance_arc(p,q).rad;
+	}
+
+	/**
+	 * Compute the angle between 'p' and 'q'
+	 * Formula from https://geophydog.cool/post/geometry_on_a_sphere/
+	 */ 
+	static azimuth(p: Position, q: Position): Angle {
+		const phi1 = lat(p).rad;
+		const phi2 = lat(q).rad;
+		const delta_lambda = lon(q).rad - lon(p).rad
+
+		const y = Math.sin(delta_lambda) * Math.cos(phi2);
+		const x = Math.cos(phi1) * Math.sin(phi2) - 
+			Math.sin(phi1) * Math.cos(phi2) * Math.cos(delta_lambda);
+		const alpha = Math.atan2(y, x);
+
+		return Angle.radian(alpha);
+	}
+
+	/**
+	 * Compute the position after a travel of 'arc' distance from 'start' heading at 'azimuth' angle
+	 * Equation from : "Map Projections - A Working Manual", page 31, equation 5-5 and 5-6. [ https://pubs.usgs.gov/pp/1395/report.pdf ]
+	 */  
+	static end_position(p: Position, azimuth: Angle, arc: Angle) {
+		// Convert degree to radians
+		const phi1 = lat(p).rad;
+		const lambda1 = lon(p).rad;
+		const az = azimuth.rad;
+		const c = arc.rad;
+
+		// Equation 5-5 p.31
+		const phi = Math.asin(Math.sin(phi1) * Math.cos(c) + 
+													Math.cos(phi1) * Math.sin(c) * Math.cos(az));
+
+		// Equation 5-6, p.31
+		const lambda = lambda1 + Math.atan2(Math.sin(c) * Math.sin(az),
+																				Math.cos(phi1) * Math.cos(c) - Math.sin(phi1) * Math.sin(c) * Math.cos(az));
+
+
+		return [Angle.radian(phi), Angle.radian(lambda)];
+	}
+}
